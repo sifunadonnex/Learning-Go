4a0419d1939a3174ef0a0c5a5a688d77793959f7
sifunadonnex <sifunado4704@gmail.com>
2025-08-06 14:59:51 +0300

    variables

--- main.go
+++ main.go
@@ -6,4 +6,18 @@ func main(){
 	fmt.Println("This is a simple Go program.")
 	fmt.Println("It prints messages to the console.")
 	fmt.Println("Goodbye!")
+	variableExample()
+}
+
+func variableExample() {
+	var message string = "This is a variable example."
+	var number int = 42
+	var isActive bool = true
+	var pi float64 = 3.14
+
+	fmt.Println(message)
+	fmt.Println("Number:", number)
+	fmt.Println("Is Active:", isActive)
+	fmt.Println("Value of Pi:", pi)
+	fmt.Println("Variable example completed.")
 }
