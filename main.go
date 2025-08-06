13c48b203ef4e8bf9589e3e669cd6e23142299c9
sifunadonnex <sifunado4704@gmail.com>
2025-08-06 14:55:05 +0300

    hellow world

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("It prints messages to the console.")
+	fmt.Println("Goodbye!")
+}
