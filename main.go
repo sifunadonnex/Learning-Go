9b6245632582e67619de2b031ad728983bcb994e
sifunadonnex <sifunado4704@gmail.com>
2025-08-06 15:14:05 +0300

    control flow

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	fmt.Println("Difference:", diff)
 	fmt.Println("Product:", prod)
 	fmt.Println("Quotient:", quot)
+	controlFlowExample()
 }
 
 func variableExample() {
@@ -44,4 +45,44 @@ func arithmeticOperations(a int, b int) (int, int, int, float64) {
 	prod := a * b
 	quot := float64(a) / float64(b)
 	return sum, diff, prod, quot
+}
+
+func controlFlowExample() {
+	// Example of control flow with if-else
+	num := 10
+	if num%2 == 0 {
+		fmt.Println(num, "is even.")
+	} else {
+		fmt.Println(num, "is odd.")
+	}
+	// Example of control flow with switch
+	switch num {
+	case 0:
+		fmt.Println("Zero")
+	case 1:
+		fmt.Println("One")
+	default:
+		fmt.Println("Greater than one")
+	}
+	// Example of control flow with for loop
+	for i := 0; i < 5; i++ {
+		fmt.Println("Iteration:", i)
+	}
+	// Example of control flow with range
+	slice := []int{1, 2, 3, 4, 5}
+	for index, value := range slice {
+		fmt.Printf("Index: %d, Value: %d\n", index, value)
+	}
+	// Example of control flow with select
+	ch := make(chan string)
+	go func() {
+		ch <- "Hello from goroutine"
+	}()
+	select {
+	case msg := <-ch:
+		fmt.Println(msg)
+	default:
+		fmt.Println("No message received")
+	}
+	fmt.Println("Control flow example completed.")
 }
